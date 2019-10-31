f715742151ce6092ebe2d56b43c1404671532e9d
Leonardo R <lfavio2@gmail.com>
2019-10-30 22:18:52 -0500

    actualizacion

---
+++ src/main/java/com/silva/book/api/AppProperties.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.silva.book.api;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+
+    private List<Menu> menus = new ArrayList<>();
+
+
+    public static class Menu {
+        private String name;
+        private String path;
+        private String title;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        
+
+        @Override
+        public String toString() {
+            return "Menu{" +
+                    "name='" + name + '\'' +
+                    ", path='" + path + '\'' +
+                    ", title='" + title + '\'' +
+                    '}';
+        }
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+
+
+    
+}
--- src/main/java/com/silva/book/api/BookController.java
+++ src/main/java/com/silva/book/api/BookController.java
@@ -2,19 +2,23 @@ package com.silva.book.api;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookController extends RouteBuilder {
 
+    @Autowired
+    private AppProperties appProperties;
+
     @Override
     public void configure() throws Exception {
         restConfiguration().component("servlet").host("localhost").port(8090).bindingMode(RestBindingMode.json);
 
         rest().get("/hello").produces(MediaType.APPLICATION_JSON_VALUE)
-                .route().setBody(constant("Bienvenido java")).endRest();
-        
+                .route().setBody(constant("Bienvenido java: "+ appProperties.getMenus())).endRest();
+
 
     }
 
